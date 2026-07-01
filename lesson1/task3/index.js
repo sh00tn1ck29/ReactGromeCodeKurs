a76806f2367f1759305d303b0ecbec9f7f113688
sh00tn1ck29 <maksimsavrygin21@gmail.com>
2026-07-01 22:33:34 +0200

    Update index.js

--- lesson1/task3/index.js
+++ lesson1/task3/index.js
@@ -3,7 +3,7 @@ const rootElem = document.querySelector('#root');
 const element = (
   <div class="greeting">
     <div class="greeting__title">Hello, world!</div>
-    <div class="greeting__text">I'm learning React</div>
+    <div class="greeting__text">I am learning React</div>
   </div>
 );
 
