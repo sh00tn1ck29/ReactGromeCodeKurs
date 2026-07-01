84a10c3133585c76cb8e54de31d040d763a3156d
sh00tn1ck29 <maksimsavrygin21@gmail.com>
2026-07-01 22:25:26 +0200

    Update index.js

--- lesson1/task3/review_build/index.js
+++ lesson1/task3/review_build/index.js
@@ -1,1 +1,1 @@
-(()=>{const e=document.querySelector("#root"),t=React.createElement("div",{className:"greeting"},React.createElement("div",{className:"greeting__title"},"Hello, world!"),React.createElement("div",{className:"greeting__text"},"I'm learning React"));ReactDOM.render(t,e)})();
+(()=>{const e=document.querySelector("#root"),t=React.createElement("div",{class:"greeting"},React.createElement("div",{class:"greeting__title"},"Hello, world!"),React.createElement("div",{class:"greeting__text"},"I'm learning React"));ReactDOM.render(t,e)})();
