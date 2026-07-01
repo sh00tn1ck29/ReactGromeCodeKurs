6c7e79fbfb1b5e72f68b57b576ca50647d9a59c0
sh00tn1ck29 <maksimsavrygin21@gmail.com>
2026-07-01 21:16:42 +0200

    Create index.js

---
+++ lesson1/task2/index.js
@@ -0,0 +1,16 @@
+const rootElem = document.querySelector('#root');
+
+const element = React.createElement('div', { className: 'greeting' }, [
+  React.createElement(
+    'div',
+     { className: 'greeting__title' },
+     'Hello, world!'
+    ),
+    React.createElement(
+    'div',
+     { className: 'greeting__text' },
+     "I'm learning React"
+    ),
+]);
+
+ReactDOM.render(element, rootElem);
