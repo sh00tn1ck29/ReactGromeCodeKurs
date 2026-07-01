cdbad8b12fb8a9b8c4be7ead8e369699f861e4d3
sh00tn1ck29 <maksimsavrygin21@gmail.com>
2026-07-01 22:15:21 +0200

    Delete lesson1/task3/dist directory

--- lesson1/task3/dist/index.js
+++
@@ -1,9 +0,0 @@
-const rootElem = document.querySelector('#root');
-const element = /*#__PURE__*/React.createElement("div", {
-  className: "greeting"
-}, /*#__PURE__*/React.createElement("div", {
-  className: "greeting__title"
-}, "Hello, world!"), /*#__PURE__*/React.createElement("div", {
-  className: "greeting__text"
-}, "I'm learning React"));
-ReactDOM.render(element, rootElem);
