436724a2d7c93e58d2d578ee6ecebeee42b9e20f
sh00tn1ck29 <maksimsavrygin21@gmail.comgit config --global user.name sh00tn1ck29git config --global user.email maksimsavrygin21@gmail.com>
2026-07-02 20:25:35 +0200

    feat: complete lesson1 task5 todo-list with working render

---
+++ lesson1/task5/todo-list/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const rootElem = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElem);
+
+root.render(<h1 className="title">Todo List</h1>);
