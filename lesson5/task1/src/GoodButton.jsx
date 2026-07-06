c6b5a90ec901ef1a367efd88cacdb41b103cd0e8
sh00tn1ck29 <maksimsavrygin21@gmail.comgit config --global user.name sh00tn1ck29git config --global user.email maksimsavrygin21@gmail.com>
2026-07-06 20:01:46 +0200

    task1

--- lesson5/task1/src/GoodButton.jsx
+++ lesson5/task1/src/GoodButton.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class GoodButton extends Component {
   render() {
     return (
-      <button class="fancy-button" onClick={() => alert('Good Job!')}>
+      <button className="fancy-button" onClick={() => alert('Good Job!')}>
         Click me!
       </button>
     );
