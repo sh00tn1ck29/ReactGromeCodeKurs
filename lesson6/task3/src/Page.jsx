09042a7c5391116f5fee99e5f217a642d10cd188
sh00tn1ck29 <maksimsavrygin21@gmail.comgit config --global user.name sh00tn1ck29git config --global user.email maksimsavrygin21@gmail.com>
2026-07-08 15:37:24 +0200

    task3

--- lesson6/task3/src/Page.jsx
+++ lesson6/task3/src/Page.jsx
@@ -10,11 +10,11 @@ const Page = () => {
       <div className="actions">
         <button
           className="btn"
-          onClick={() => setProductInfo('Price is 500$.Available in 2 colors')}
+          onClick={() => setProductInfo('Price is 500$. Available in 2 colors')}
         >
           IPhone 13
         </button>
-        <button className="btn" onClick={() => setProductInfo('Price is 650$.Not available')}>
+        <button className="btn" onClick={() => setProductInfo('Price is 650$. Not available')}>
           IPhone 13 Pro
         </button>
         <button className="btn" onClick={() => setProductInfo('')}>
