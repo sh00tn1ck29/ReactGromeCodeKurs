01d35642f24d408b64be81bd330d78710c038ef9
sh00tn1ck29 <maksimsavrygin21@gmail.comgit config --global user.name sh00tn1ck29git config --global user.email maksimsavrygin21@gmail.com>
2026-07-09 19:32:14 +0200

    Complete lifecycle methods task

---
+++ lesson8/task1/src/Life.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class Life extends Component {
+  constructor(props) {
+    super(props);
+    console.log('constructor: good place to create state');
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount: API calls, subscriptions');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
+    return true;
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount(): cleanup before DOM related to component will be removed');
+  }
+
+  render() {
+    console.log('render(): return React element to build DOM');
+    return (
+      <div className="clock">
+        <div className="clock__location">New York</div>
+        <div className="clock__time">7:00:51 AM</div>
+      </div>
+    );
+  }
+}
+
+export default Life;
