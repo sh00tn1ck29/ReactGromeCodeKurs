5a3e1de98646e1ce572ad69ae2a9eaf8e0ec9b93
sh00tn1ck29 <maksimsavrygin21@gmail.comgit config --global user.name sh00tn1ck29git config --global user.email maksimsavrygin21@gmail.com>
2026-07-10 17:51:05 +0200

    task5

---
+++ lesson9/task5/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.scss';
+import ReactDOM from 'react-dom';
+import Game from './Game';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Game />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
