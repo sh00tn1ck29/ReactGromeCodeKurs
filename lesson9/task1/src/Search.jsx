a026c86fb7b9f9158d14e1bad073d86eb2980fcb
sh00tn1ck29 <maksimsavrygin21@gmail.comgit config --global user.name sh00tn1ck29git config --global user.email maksimsavrygin21@gmail.com>
2026-07-10 15:38:19 +0200

    task1

--- lesson9/task1/src/Search.jsx
+++ lesson9/task1/src/Search.jsx
@@ -17,16 +17,14 @@ class Search extends Component {
 
   render() {
     return (
-      <form className="search">
+      <form className="search" onSubmit={this.handleclick}>
         <input
           onChange={this.handlechange}
           type="text"
           class="search__input"
           value={this.state.value}
         />
-        <button className="search__button" onClick={this.handleclick}>
-          Search
-        </button>
+        <button className="search__button">Search</button>
       </form>
     );
   }
